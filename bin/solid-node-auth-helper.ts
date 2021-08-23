cf191e80572d534fe725c5221fd4d73b9f24ab94
Ruben Taelman <ruben.taelman@ugent.be>
2021-08-23 13:37:18 +0200

    Fix Node process hanging

--- bin/solid-node-auth-helper.ts
+++ bin/solid-node-auth-helper.ts
@@ -5,6 +5,7 @@ import { login } from '../lib/Util';
 login({ oidcIssuer: 'https://solidcommunity.net/', tokenType: 'Bearer' })
   .then(session => {
     console.log(`Logged in as ${session.info.webId}`);
+    return session.logout();
   })
   .catch(error => {
     console.log(error);
